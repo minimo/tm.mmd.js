d2d83a7881628e405a4b98566a2245713e184b33
minimo <minimo.mobile@gmail.com>
2015-07-23 15:48:54 +0900

    add MMD sequencer class

---
+++ tm.mmd.sequencer.js
@@ -0,0 +1,12 @@
+(function() {
+    tm.hybrid = tm.hybrid || {};
+
+    tm.define("tm.hybrid.MMDSequencer", {
+        superClass: "tm.event.EventDispatcher",
+
+        init: function(path) {
+            this.superInit();
+        },
+    });
+})();
+
