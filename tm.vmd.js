08b2718439002617af1257f3f7076c9e97215755
minimo <minimo.mobile@gmail.com>
2015-07-24 22:49:24 +0900

    update parse vmd file

--- tm.vmd.js
+++ tm.vmd.js
@@ -5,21 +5,77 @@
         superClass: "tm.event.EventDispatcher",
 
         init: function(path) {
-            this.data = null;
             this.superInit();
-            this.load(path);
+
+            this.vmd = null;
+            this.path = path;
+            this.loadFromURL(path);
         },
 
-        load: function(path) {
-            var onProgress = function(xhr) {};
-            var onError = function(xhr) {};
-
+        // URLからロード
+        loadFromURL: function(path) {
             var that = this;
-            var loader = new THREE.MMDLoader();
-            loader.loadFileAsBuffer(path, function(buffer) {
-                that.data = loader.parseVmd(buffer);
-                that.flare("load");
-            }, onProgress, onError);
+            var req = new XMLHttpRequest();
+            req.open("GET", path, true);
+            req.responseType = "arraybuffer";
+            req.onload = function() {
+                var data = req.response;
+                that.loadFromData(data);
+            };
+            req.send(null);
+        },
+
+        //データからロード
+        loadFromData: function(data) {
+            this.dv = tm.DataViewEx(data);
+            this.offset = 0;
+            this.pmd = this._parse(data);
+            this.flare("load");
+        },
+
+        _parse: function(data) {
+            var dv = this.dv;
+            var vmd = {};
+            vmd.metadata = {};
+            vmd.metadata.format = 'vmd';
+            var metadata = vmd.metadata;
+
+            // Header
+            metadata.magic = dv.getChars(30);
+            if (metadata.magic !== 'Vocaloid Motion Data 0002') {
+                console.warn("{0}はVMDじゃないよ".format(this.path));
+                vmd.result = "This File is not VMD.";
+                vmd.error = true;
+            }
+            metadata.name = dv.getSjisStrings(20);
+
+            // Motion
+            metadata.motionCount = dv.getUint32();
+            vmd.motions = [];
+            for (var i = 0; i < metadata.motionCount; i++) {
+                var m = {};
+                m.boneName = dv.getSjisStrings(15);
+                m.frameNum = dv.getUint32();
+                m.position = [dv.getFloat32(), dv.getFloat32(), dv.getFloat32()];
+                m.rotation = [dv.getFloat32(), dv.getFloat32(), dv.getFloat32(), dv.getFloat32()];
+                m.interpolation = [];
+                for ( var i = 0; i < 64; i++ ) {
+                    m.interpolation.push(dv.getUint8());
+                }
+                vmd.motions.push(m);
+            }
+
+            // Morphing
+            metadata.morphCount = dv.getUint32();
+            vmd.morphs = [];
+            for (var i = 0; i < metadata.morphCount; i++) {
+                var m = {};
+                m.morphName = dv.getSjisStrings(15);
+                m.frameNum = dv.getUint32();
+                m.weight = dv.getFloat32();
+                vmd.morphs.push(m);
+            }
+            return vmd;
         }
     });
 
